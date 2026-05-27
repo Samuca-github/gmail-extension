42a8023b6e31224764bbb8ca07b84ca8a7474500
SAMUEL SANTOS DE SOUZA <81575418+Samuca-github@users.noreply.github.com>
2026-05-27 19:56:13 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
-  ANALYZE_API_URL: "http://localhost:8080/analyze",
+  ANALYZE_API_URL: "https://api-analize.onrender.com/analyze",
   SUPABASE_URL: "https://fmpsqygpdujikcqsuvke.supabase.co",
   SUPABASE_ANON_KEY:
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZtcHNxeWdwZHVqaWtjcXN1dmtlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0MDY1NDksImV4cCI6MjA5Mjk4MjU0OX0.Cep5oVgyc5-OY2IY48ntnyve5GQnTfYg3KRhDEHFIEM",
